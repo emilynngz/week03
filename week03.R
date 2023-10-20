e37636ff9c8dcf7293610ff3639c80221ec94034
emilynngz <emilyningzizhu@outlook.com>
2023-10-20 13:04:21 +0100

    Update week03.R

--- week03.R
+++ week03.R
@@ -1,1 +1,8 @@
 emdat<- read.csv("EMDAT.csv")
+pacman::p_load(
+  tidyverse,
+  skimr
+)
+df <- emdat %>% select (Entity, Year, deaths_all_disasters, injured_all_disasters,
+                        homeless_all_disasters)
+
